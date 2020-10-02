af3cc0f68e3bfabcb49116c07f04b30b5e206879
microcheese <ernst.reiter@microcheese.de>
2020-10-02 16:40:18 +0200

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -1,18 +1,15 @@
-// swift-tools-version:4.2
- import PackageDescription
+// swift-tools-version:5.0
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+import PackageDescription
 
- let package = Package(
-     name: "DropDown",
-     platforms: [.iOS(.v8)],
-     products: [
-         .library(name: "DropDown", targets: ["DropDown"])
-     ],
-     dependencies: [],
-     targets: [
-         .target(
-             name: "DropDown",
-             dependencies: [],
-             path: "DropDown"
-         )
-     ]
- )
+let package =   Package(
+  name: "DropDown",
+  platforms: [.iOS(.v8)],
+  products: [
+    .library(name: "DropDown", targets: ["DropDown"]),
+  ],
+  targets: [
+    .target(name: "DropDown", path: "DropDown")
+  ],
+  swiftLanguageVersions: [.v5]
+)
